6c4339a80b3e4c40642f21303775b9b0717cb97f
shin <finetonight@gmail.com>
2022-07-11 21:28:16 +0900

    mongoDB 사용하기

---
+++ coding-apple/node/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+const MongoClient = require("mongodb").MongoClient;
+const mongoAddress =
+  "mongodb+srv://hanshin:hanshin@atlascluster.vpb5laj.mongodb.net/?retryWrites=true&w=majority";
+MongoClient.connect(mongoAddress, function (error, client) {
+  if (error) return console.log("서버 열림");
+  db = client.db("todoapp");
+
+  app.post("/add", function (req, res) {
+    res.send("완료");
+    console.log(req.body);
+    db.collection("count").findOne(
+      { title: "boardCount" },
+      function (err, result) {
+        console.log(result);
+        var totalPost = result.totalPost;
+        db.collection("post").insertOne(
+          { _id: totalPost + 1, title: req.body.title, date: req.body.date },
+          function (err2, result2) {
+            console.log("저장완료");
+            db.collection("count").updateOne(
+              { title: "boardCount" },
+              { $inc: { totalPost: 1 } },
+              function (err2, result3) {
+                if (err2) {
+                  return console.log(err);
+                }
+              }
+            );
+          }
+        );
+      }
+    );
+  });
+});
+var db;
+
+app.listen(8080, function () {});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+
+app.get("/list", function (req, res) {
+  db.collection("post")
+    .find()
+    .toArray(function (err, result) {
+      console.log(result);
+      res.render("list.ejs", { posts: result });
+    });
+});
--- oncare/theme/onmedi/js/main.js
+++ oncare/theme/onmedi/js/main.js
@@ -22,9 +22,91 @@ $(function () {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    variableWidth: false,
+    variableWidth: true,
     appendDots: $visualCtrl,
     prevArrow: $visualPrev,
     nextArrow: $visualNext,
+    centerMode: true,
+  });
+
+  //추천제품 탭
+  var $rcmd = $(".recommend"),
+    $rcmdNavBtn = $rcmd.find(".tab_nav .tab_btn"),
+    $rcmdCon = $rcmd.find(".tab_con_list");
+
+  $rcmdNavBtn
+    .on("click", function () {
+      var $this = $(this),
+        thisIndex = $this.index();
+      $rcmdNavBtn.removeClass("active");
+      $this.addClass("active");
+      $rcmdCon.removeClass("active");
+      $rcmdCon.eq(thisIndex).addClass("active");
+    })
+    .triggerHandler("click");
+
+  //견적서
+  var $estimateSlideWrap = $(".estimate_slide"),
+    $estimateSlide = $estimateSlideWrap.find(".estimate_list"),
+    $estimateItem = $estimateSlide.find(".estimate_item"),
+    $estimateDots = $estimateSlideWrap.find(".estimate_ctrl .dots"),
+    nextIndex = 0;
+
+  var $estimate2Wrap = $(".estimate2_slide_list"),
+    $estimate2SlideWrap = $estimate2Wrap.find(".estimate2_slide");
+  $estimateSlide.slick({
+    autoplay: false,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: $estimateDots,
+    customPaging: function (slider, i) {
+      return (
+        '<button type="button" data-index="' +
+        i +
+        '">' +
+        (i + 1) +
+        "/" +
+        slider.slideCount +
+        "</button>"
+      );
+    },
+  });
+  $estimateSlide.on("beforeChange", function (slide, i, crr, next) {
+    nextIndex = next;
+  });
+
+  $estimate2SlideWrap.each(function () {
+    $(this).find(".estimate2_list").slick({
+      autoplay: false,
+      dots: false,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      swipeToSlide: true,
+    });
+  });
+  $estimateItem
+    .find("button")
+    .on("click", function () {
+      $estimate2SlideWrap
+        .eq(nextIndex)
+        .addClass("active")
+        .siblings()
+        .removeClass("active");
+    })
+    .triggerHandler("click");
+  $(document).on("click", ".estimate_slide .dots button", function () {
+    var $this = $(this),
+      thisIndex = $this.attr("data-index");
+    $estimate2SlideWrap
+      .eq(thisIndex)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
   });
 });
